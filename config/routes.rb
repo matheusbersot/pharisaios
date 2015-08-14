e1cf541c7525ae90f481cbbae159ce140aa078f7
Matheus Bersot <matheusbersot@id.uff.br>
2015-08-13 22:23:34 -0300

    REFS #4 por Matheus Bersot: Criar webservices para acesso aos dados

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'moeda/obterValoresDeUmaMoeda/:sigla_moeda' => 'moeda#obterValoresDeUmaMoeda'
+  get 'moeda/obterTudo' => 'moeda#obterValoresDeTodasMoedas'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
